b6e0000868e6ab6280d8309f348b9fe07b0cb0a7
laura1405 <161332723+laura1405@users.noreply.github.com>
2025-04-24 14:26:22 +0000

    add aula 24/04

---
+++ src/pages/esportes.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+export default function Esportes() {
+return (
+  <Container>
+    <Row>
+      <Col>Bem vindo a Esportes </Col>
+    </Row>
+  </Container>
+   );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+export default function Home() {
+return (
+  <Container>
+    <Row>
+      <Col>Bem vindo a FW </Col>
+    </Row>
+  </Container>
+   );
+}
