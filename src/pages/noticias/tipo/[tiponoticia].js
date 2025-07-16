6b603382787dc40135310cc715f8442a65394d5a
laura1405 <161332723+laura1405@users.noreply.github.com>
2025-07-16 15:36:21 +0000

    API

---
+++ src/pages/noticias/tipo/[tiponoticia].js
@@ -0,0 +1,23 @@
+import { Container, Row } from 'react-bootstrap';
+import Cards from '../../components/cards';
+export async function getServerSideProps(req) {
+  // Fetch data from external API
+  const res = await fetch('https://portalnoticias-delta.vercel.app/api/noticias/tipo/'+req.query.tiponoticia)
+  const repo = await res.json()
+  // Pass data to the page via props
+  return { props: { noticias: repo } }
+}
+export default function Page({ noticias }) {
+  return (
+    <Container>
+      <Row xs={1} md={2} lg={3} className="g-4 pt-2">
+        {Array.isArray(noticias) ?
+          noticias.map(noticia =>
+            <Cards idnoticia={noticia.idnoticia} titulonoticia={noticia.titulonoticia}
+              tiponoticia={noticia.tiponoticia} conteudonoticia={noticia.conteudonoticia}
+              datahoracadastro={noticia.datahoracadastro} />
+          ): "falso"}
+      </Row>
+    </Container>
+  );
+}
